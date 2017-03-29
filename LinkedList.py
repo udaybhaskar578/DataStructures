c8d92cc77d9a7d273d1dfd3eb9e0cf84751373d1
Sai Uday Bhaskar Mudivarty <narenuday@Sais-MacBook-Pro.local>
2017-03-29 16:10:49 -0700

    Insert Element at Nth Position

--- LinkedList.py
+++ LinkedList.py
@@ -11,6 +11,7 @@ class LinkedList:
     def __init__(self):
         self.root = None
         self.size = 0
+
     def add(self,data):
         newNode = Node(data,None)
         if self.root == None:
@@ -25,7 +26,6 @@ class LinkedList:
     def delete(self,data):
         isFound = False
         currentNode = self.root
-        self.printList()
         while not isFound and currentNode != None:
             if currentNode.data == data:
                 if currentNode == self.root:
@@ -35,6 +35,7 @@ class LinkedList:
                 del currentNode
                 self.size = self.size -1
                 isFound = True
+                print ("Deleted "+str(data)+" from the linked list")
                 break
                 
             prev = currentNode
@@ -51,6 +52,37 @@ class LinkedList:
             currentNode = currentNode.next
             i+=1
     
+    def search(self,data):
+        currentNode = self.root
+        while currentNode !=None:
+            if currentNode.data == data:
+                print("Found "+str(data)+" in the linked list")
+                return currentNode
+            currentNode = currentNode.next
+        print(str(data)+" is not found in the linked list")
+    
+    def addAsRoot(self,data):
+        newNode = Node(data,self.root)
+        self.root = newNode
+    
+    def addAtNthPosition(self,data,n):
+        try:        
+            if n ==1:
+                self.addAsRoot(data)
+            elif n > self.__len__()+1:
+                raise StandardError()
+            else:
+                
+                currentNode = self.root
+                for i in range(1,n-1):
+                    currentNode = currentNode.next
+                newNode = Node(data,currentNode.next)
+                currentNode.next = newNode
+                
+        except StandardError:
+            print("The size of the linked list is less than the size of the desired position")
+            print("Unable to Add "+str(data)+ " at "+str(n)+" position.")
+        
     def __len__(self):
         return self.size
     
@@ -59,8 +91,19 @@ if __name__ == '__main__':
     ll = LinkedList()
     ll.add(10)
     ll.add(12)
+    ll.add(14)
+    ll.add(100)
+    ll.printList()
+    print("")
+    ll.addAtNthPosition(101,5)
+    ll.printList()
+    ll.addAsRoot(1)
+    print("")
+    ll.printList()
+    ll.search(14)
     ll.delete(14)
-    print(len(ll))
+    ll.search(14)
+
 
 
 
