a150d5fe00409b10dcf6e1c2d8b486bfd8117783
Sai Uday Bhaskar Mudivarty <narenuday@Sais-MacBook-Pro.local>
2017-04-13 13:39:53 -0700

    Min,Max,Height of a BST

--- BinarySearchTree.py
+++ BinarySearchTree.py
@@ -8,75 +8,105 @@ class Node:
         self.data = data
         self.left = left
         self.right = right
-    
-    @property
-    def data(self):
-        return self.data
-
-    @data.setter
-    def data(self, data):
-        self.data = data
-
-    @property
-    def left(self):
-        return self.left
-
-    @left.setter
-    def left(self, left):
-        self.left = left
-
+        
 class BinarySearchTree:
     def __init__(self):
         self.root = None
         self.height = 0
 
-    @property
-    def root(self):
-        return self.root
-    @root.setter
-    def root(self, root):
-        self.root = root
-
-    @property
-    def height(self):
-        return self.height
-
-    @height.setter
-    def height(self, height):
-        self.height = height
-    
-    def Insert(self,root,data):
-        if root == None:
-            root = Node(data)
-        elif data <= root.data:
-            root.left = Insert(root.left,data)
+    def Insert(self,data):
+        if self.root == None:
+            self.root = Node(data)
         else:
-            root.right = Insert(root.right,data)
-        return root
+            self._Insert(self.root,data)
+
+    def _Insert(self,node,data):
+        if data <= node.data:
+            if node.left ==None:
+                node.left = Node(data)
+            else:
+                self._Insert(node.left,data)
+        else:
+            if node.right == None:
+                node.right = Node(data)
+            else:
+                self._Insert(node.right,data)
     
+    def println(self):
+        print("J+Holea")
+    def SearchTree(self,root,data):
+        if root == None:
+            return False
+        elif data == root.data:
+            return True
+        elif data <= root.data:
+            self.SearchTree(root.left,data)
+        else:
+            self.SearchTree(root.right,data)
+
     def Search(self,root,data):
         if root == None:
             return False
         elif data == root.data:
             return True
         elif data <= root.data:
-            Search(root.left,data)
+            # print("Searching Further Left to "+str(root))
+            return self.Search(root.left,data)
         else:
-            Search(root.right,data)
+            return self.Search(root.right,data)
+        
+    def _FindMin(self,root):
+        if root == None:
+            return -1
+        elif root.left == None:
+            return root.data
+        return self._FindMin(root.left)
+    
+    def FindMin(self):
+        if self.root == None:
+            return -1
+        else:
+            return self._FindMin(self.root)
+
+    def _FindMax(self,root):
+        if root == None:
+            return -1
+        elif root.right == None:
+            return root.data
+        return self._FindMax(root.right)
+    
+    def FindMax(self):
+        if self.root == None:
+            return -1
+        else:
+            return self._FindMax(self.root)
+    def _HeightOfTree(self,root):
+        if root == None:
+            return -1
+        left = self._HeightOfTree(root.left)
+        right = self._HeightOfTree(root.right)
+
+        return max(left,right)+1
+
+    def HeightOfTree(self):
+        if self.root == None:
+            return -1
+        else:
+            return self._HeightOfTree(self.root)
 
 if __name__ == "__main__":
     bst = BinarySearchTree()
-    bst.root == bst.Insert(bst.root,15)
-    print("After Initialization"+bst.root)
-    bst.root == bst.Insert(bst.root,12)
-    bst.root == bst.Insert(bst.root,10)
-    bst.root == bst.Insert(bst.root,18)
-    bst.root == bst.Insert(bst.root,19)
-    bst.root == bst.Insert(bst.root,25)
-    bst.root == bst.Insert(bst.root,21)
-    bst.root == bst.Insert(bst.root,17)
-
-
-    print(bst.Search(bst.root,16))
-    print(bst.Search(bst.root,17))
-
+    bst.Insert(15)
+    # print("After Initialization"+str(bst.root.data))
+    bst.Insert(12)
+    # print("After Initialization"+str(bst.root.left.data))
+    bst.Insert(10)
+    # print("After Initialization"+str((bst.root.left).left.data))
+    bst.Insert(19)
+    bst.Insert(25)
+    bst.Insert(21)
+    bst.Insert(17)    
+    print(bst.Search(bst.root,22))
+    print(bst.FindMax())
+    print(bst.FindMin())
+    print(bst.HeightOfTree())
